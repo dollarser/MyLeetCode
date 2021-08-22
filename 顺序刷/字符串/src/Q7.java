12cdc15b6d4119fe5ccb5fc1dbd441796bfe6206
dollarser <dollarser@126.com>
2021-08-22 18:27:41 +0800

    添加KMP

--- 顺序刷/字符串/src/Q7.java
+++ 顺序刷/字符串/src/Q7.java
@@ -1,6 +1,27 @@
+/**
+ * 459.重复的子字符串
+ * https://leetcode-cn.com/problems/repeated-substring-pattern/
+ *
+ * 给定一个非空的字符串，判断它是否可以由它的一个子串重复多次构成。给定的字符串只含有小写英文字母，并且长度不超过10000。
+ *
+ * 示例 1:
+ * 输入: "abab"
+ * 输出: True
+ * 解释: 可由子字符串 "ab" 重复两次构成。
+ *
+ * 示例 2:
+ * 输入: "aba"
+ * 输出: False
+ *
+ * 示例 3:
+ * 输入: "abcabcabcabc"
+ * 输出: True
+ * 解释: 可由子字符串 "abc" 重复四次构成。 (或者子字符串 "abcabc" 重复两次构成。)
+ */
 public class Q7 {
     public static void main(String[] args) {
-        String s = "abab";
+        //String s = "abac";
+        String s = "abacababacab";
         Solution solution = new Solution();
         boolean ans = solution.repeatedSubstringPattern(s);
         System.out.println(ans);
@@ -9,23 +30,31 @@ public class Q7 {
         public boolean repeatedSubstringPattern(String s) {
             int[] next = new int[s.length()];
             int i = 1, j = 0;
-            next[i] = -1;
+            next[0] = -1;
             //标准前缀
-            while( i < s.length()-1) {
-                if (s.charAt(i) == s.charAt(j)) {
+            while( i < s.length() ) {
+                if (j == -1 || s.charAt(i) == s.charAt(j)) {
                     next[i] = j;
                     ++i;
                     ++j;
-                } else if( j == 0 &&  s.charAt(i) != s.charAt(j)) {
-                    next[i] = j;
-                    ++i;
                 } else {
-                    j = next[j];
+                    /**
+                     * 回退是按前一个的next回退
+                     */
+                    j--;
+                    if( j >= 0 ) j = next[j]+1;
                 }
             }
             int k = s.length() - 1;
-            System.out.println(next[s.length()-1]);
-            if(s.length() % (s.length() - next[s.length()-1] - 1) == 0) {
+            for (int l: next) {
+                System.out.println(l);
+            }
+            /**
+             * 判断是否有循环子字串:
+             * 原理 最终串比模式串多的字符如果能被串的长度整除则说明是重复子字串
+             */
+            if(s.length() % (s.length()-1 - next[s.length()-1]) == 0 &&
+                    s.length() != s.length()-1 - next[s.length()-1] ) {
                 return true;
             }
             return false;
---
+++ 顺序刷/栈与队列/src/Q1.java
@@ -0,0 +1,80 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 232.用栈实现队列
+ * https://leetcode-cn.com/problems/implement-queue-using-stacks/
+ *
+ * 使用栈实现队列的下列操作：
+ *
+ * push(x) -- 将一个元素放入队列的尾部。
+ * pop() -- 从队列首部移除元素。
+ * peek() -- 返回队列首部的元素。
+ * empty() -- 返回队列是否为空。
+ *
+ * 示例:
+ *
+ * MyQueue queue = new MyQueue();
+ * queue.push(1);
+ * queue.push(2);
+ * queue.peek();  // 返回 1
+ * queue.pop();   // 返回 1
+ * queue.empty(); // 返回 false
+ * 说明:
+ *
+ * 你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+ * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+ * 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+ */
+public class Q1 {
+    public static void main(String[] args) {
+        int x = 0;
+        MyQueue solution = new MyQueue();
+        solution.push(x);
+        System.out.println();
+    }
+
+    static class MyQueue {
+        // java中的 Stack 有设计上的缺陷，官方推荐使用 Deque(双端队列) 代替 Stack
+        Deque<Integer> stIn;
+        Deque<Integer> stOut;
+        /** Initialize your data structure here. */
+        /** Initialize your data structure here. */
+        public MyQueue() {
+            stIn = new ArrayDeque<>();
+            stOut = new ArrayDeque<>();
+        }
+
+        /** Push element x to the back of queue. */
+        public void push(int x) {
+            stIn.push(x);
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int pop() {
+            // 只要 stOut 为空，那么就应该将 stIn 中所有的元素倒腾到 stOut 中
+            if (stOut.isEmpty()) {
+                while (!stIn.isEmpty()) {
+                    stOut.push(stIn.pop());
+                }
+            }
+            // 再返回 stOut 中的元素
+            return stOut.pop();
+        }
+
+        /** Get the front element. */
+        public int peek() {
+            // 直接使用已有的pop函数
+            int res = this.pop();
+            // 因为pop函数弹出了元素res，所以再添加回去
+            stOut.push(res);
+            return res;
+        }
+
+        /** Returns whether the queue is empty. */
+        public boolean empty() {
+            return stIn.isEmpty() && stOut.isEmpty();
+        }
+    }
+
+}
